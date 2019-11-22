ec1e2d949319122e961af81d362110d069bcd6a1
Aaron Allehoff <51172401+aallehoff@users.noreply.github.com>
2019-11-21 19:29:17 -0500

    Continue work on app logic

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
+let question = 0;
+let response = {
+  "personality": "",
+  "rooms": 0,
+  "yard": false,
+  "has": []
+};
+
+// Wait for user input to begin.
+$('.appInitial').on('click', 'button', (event) => {
+  // Run the function named in button.
+  window[event.target.attributes['data-selection'].nodeValue]();
+})
+
 function viewAll() {
   console.log(true)
 }
@@ -6,6 +20,20 @@ function petMatch() {
   console.log(false);
 }
 
-$('.appInitial').on('click', (event) => {
-  window[event.target.attributes['data-selection'].nodeValue]();
-})
+
+
+
+let pets;
+
+$(document).ready(function() {
+console.log(pets)
+  $.getJSON('js/data.json', (result) => {
+    console.log(result);
+    console.log('in ' + result);
+    pets = result;
+  }).done((result) => {
+    console.log('done ' + result)
+  })
+
+  console.log('after ' + pets);
+});
