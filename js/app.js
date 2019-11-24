20b6c48154b07b41eaf05164eb189bc0c71f7f55
Aaron Allehoff <51172401+aallehoff@users.noreply.github.com>
2019-11-24 02:48:26 -0500

    Continue question logic and add data-input attr to app HTML

--- js/app.js
+++ js/app.js
@@ -1,12 +1,7 @@
 /* eslint-env es6, browser, jquery */
 
 let question = 0;
-let response = {
-  "personality": "",
-  "rooms": 0,
-  "yard": false,
-  "has": []
-};
+let response = [];
 
 // Wait for user input to begin.
 $('.appInitial').on('click', 'button', (event) => {
@@ -15,6 +10,7 @@ $('.appInitial').on('click', 'button', (event) => {
 })
 
 function viewAll() {
+  question = -1;
   console.log("viewAll: Not yet implemented");
 }
 
@@ -22,9 +18,20 @@ function petMatch() {
   question += 1;
   $('.appInitial').hide();
   $('#q1').show();
-  
+
   $('.appQuestion').on('click', 'button', (event) => {
-    
+    if (event.target.attributes['data-input'] !== undefined) {
+      console.log("Input: Not yet implemented")
+    } else {
+      response[question] = event.target.attributes['data-selection'].nodeValue;
+      console.log(response);
+      $('#q' + question).hide();
+      question++;
+      $('#q' + question).show();
+
+    }
+
+
   })
 }
 
