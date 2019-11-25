409a6e197283b5abd0b5a059364f6728fa3359d2
aallehoff <51172401+aallehoff@users.noreply.github.com>
2019-11-25 05:45:32 -0500

    Cleanup work flow and add comments

--- js/app.js
+++ js/app.js
@@ -3,20 +3,20 @@
 let question = 0;
 let response = [];
 
-// Wait for user input to begin.
-$('.appInitial').on('click', 'button', (event) => {
-  // Run the function named in button.
-  window[event.target.attributes['data-selection'].nodeValue]();
-})
-
-function viewAll() {
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  BEGIN .appInitial #viewAll onClick
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+$('.appInitial').on('click', '#viewAll', () => {
+  // Hide .appIntial and set values for debug.
   $('.appInitial').hide();
   question = -1;
   response[question] = "viewAll";
 
-  $.getJSON('js/data.json', (result) => {
-    $.each(result, (i, petObj) => {
-      console.log(i, petObj);
+  // Fetch JSON data and print it to div.appOutput.
+  $.getJSON('js/data.json', (jsonData) => {
+    // Perform action on each object in the asynchronously returned array.
+    $.each(jsonData, (i, petObj) => {
+      // Template output.
       let output = `<div class="card">
                       <div class="card-body">
                         <img src="${petObj['content']['imgSrc']}" class="card-img-top" alt="${petObj['content']['name']} the ${petObj['content']['description']} ${petObj['content']['species']}">
@@ -29,50 +29,59 @@ function viewAll() {
                         <li class="list-group-item">Sex: ${petObj['content']['sex']}</li>
                       </ul>
                     </div>`;
+      // Append output.
       $('.appOutput').append(output);
 
     });
   })
+}); // END .appInitial #viewAll onClick
 
-}
-
-function petMatch() {
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  BEGIN .appInitial #petMatch onClick
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+$('.appInitial').on('click', '#petMatch', () => {
+  //Hide .appInitial and set values to start questions.
   $('.appInitial').hide();
   response[question] = "petMatch";
   question = 1;
   $('#q1').show();
 
-  function nextQuestion() {
-    console.log(response);
-    $('#q' + question).hide();
-    question++;
-    $('#q' + question).show();
-  }
-
+  // For each click of a button...
   $('.appQuestion').on('click', 'button', (event) => {
+    // Immediately hide question.
+    $('#q' + question).hide();
+    
+    // Record values into response.
     if (event.target.attributes['data-input'] !== undefined) {
+      // If data-input attribute is present...
       for (let i = 0; i < $(event.target).siblings('input').length; i++) {
+        // ...for each sibling...
         if ($(event.target).siblings('input')[i].attributes['type'].nodeValue == "number") {
+          // ...if input type is number, record value of input into response.
           response[question] = $($(event.target).siblings('input')[i]).val();
         } else if ($(event.target).siblings('input')[i].attributes['type'].nodeValue == "checkbox") {
-          //          console.log('Checkbox: not yet implemented');
+          // ...else if input type is checkbox...
           if (!$.isArray(response[question])) {
+            // ...create an array...
             response[question] = [];
           }
+          // ...then record the value of each checked box into response.
           if ($($(event.target).siblings('input')[i]).is(':checked')) {
             response[question].push(
               $(event.target).siblings('input')[i].attributes['data-selection'].nodeValue
             );
           }
-
         }
       }
     } else {
+      // Otherwise, record value of data-selection attribute into response.
       response[question] = event.target.attributes['data-selection'].nodeValue;
-
     }
 
-    nextQuestion();
+    // Increment counter and display next question.
+    question++;
+    $('#q' + question).show();
 
   })
-}
+
+}); // END .appInitial #petMatch onClick
